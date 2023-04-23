63add402909ac1ea62eff4dd7d3826eb75c19296
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-04-23 19:42:25 -0400

    navbar and directory created

---
+++ priyanka-agrawal-portfolio/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function MainNav() {
+    return (
+      <>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">Priyanka Agrawal</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#about-me">About Me</Nav.Link>
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#resume">Resume</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
+
+  export default MainNav;
---
+++ priyanka-agrawal-portfolio/src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainNav from "./Navbar";
+
+function Portfolio() {
+    return (
+        <>
+            <MainNav />
+        </>
+    );
+}
+
+export default Portfolio;
